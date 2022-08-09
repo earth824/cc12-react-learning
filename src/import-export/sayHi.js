6a241a786b497c0a5eee61cca83caf4f973bf0dc
pisutthee <pisut.flyinggiraffe@gmail.com>
2022-08-09 11:30:02 +0700

    finish learning import export

---
+++ src/import-export/sayHi.js
@@ -0,0 +1,30 @@
+// function sayHi() {
+//   console.log('Say Hi');
+// }
+
+// function sayHello() {
+//   console.log('Say Hello');
+// }
+
+// const month = ['Jan', 'Feb', 'Mar', 'Apr'];
+
+// // Default Export (1 per module)
+// export default sayHi;
+
+// // Name Export (many per module)
+// export { sayHello, month };
+
+// export default function sayHi() {
+//   console.log('Say Hi');
+// }
+
+export default function () {
+  console.log('Say Hi');
+}
+
+export const month = ['Jan', 'Feb', 'Mar', 'Apr'];
+export function sayHello() {
+  console.log('Say Hello');
+}
+export const day = ['mon', 'tue', 'wed'];
+export const year = [2022, 2023, 2024];
--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Default Import ชื่อเป็นอะไรก็ได้
+import abcd from './import-export/sayHi';
+
+// Name Import ชื่อต้องตรงกับที่ export ออกมา
+import { month as mth } from './import-export/sayHi';
+
+import { month, sayHello, day, year } from './import-export/sayHi';
+// import everything as object
+import * as obj from './import-export/sayHi';
+
+// obj.sayHello()
+// obj.year;
+
+// abcd();
+// console.log(mth);
+
+month.push('May');
+console.log(mth);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
