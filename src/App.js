869cbad8244156c35d0a9a278791807d5130602a
pisutthee <pisut.flyinggiraffe@gmail.com>
2022-08-10 12:10:42 +0700

    solve lab 1,2

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center' }}>
+      <h1>********** LAB 1 **********</h1>
+      <input type="text" onChange={event => console.log(event.target.value)} />
+      <h1>********** LAB 2 **********</h1>
+      <select
+        onChange={event => {
+          console.dir(event.target);
+          console.log(event.target.value);
+        }}
+      >
+        <option value="LAO">Lao</option>
+        <option value="MYANMAR">Myanmar</option>
+        <option value="CAMBODIA">Cambodia</option>
+        <option value="MALAYSIA">Malaysia</option>
+        <option value="THAILAND">Thailand</option>
+      </select>
+      <h1>********** LAB 3 **********</h1>
+      <label>Apple</label>
+      <input type="checkbox" name="phoneBrand" value="Apple" />
     </div>
   );
 }
