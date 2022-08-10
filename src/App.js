0338f352a6f58da8eb8273e7f5a50dc0f3162ab6
pisutthee <pisut.flyinggiraffe@gmail.com>
2022-08-10 17:43:57 +0700

    solve 1,2,3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // Lab 1
+  const [isShow1, setIsShow1] = useState(true);
+
+  // Lab 2
+  const [isShow2, setIsShow2] = useState(true);
+
+  // Lab 3
+  const [isShow3, setIsShow3] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>********** Lab 1 **********</h1>
+      {isShow1 && (
+        <button onClick={() => setIsShow1(false)}>Click to hide me</button>
+      )}
+
+      <h1>********** Lab 2 **********</h1>
+      <button onClick={() => setIsShow2(false)}>Click to hide text</button>
+      {isShow2 && <h1>Text</h1>}
+
+      <h1>********** Lab 3 **********</h1>
+      <button onClick={() => setIsShow3(!isShow3)}>
+        {isShow3 ? 'Hide' : 'Show'}
+      </button>
+      {isShow3 && <h1>Lorem Ipsom</h1>}
     </div>
   );
 }
