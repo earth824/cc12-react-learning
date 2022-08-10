704409504642594e212ad7aca7dffcee9834c9ea
pisutthee <pisut.flyinggiraffe@gmail.com>
2022-08-10 10:24:24 +0700

    finish learning css styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import classes from './App.module.css';
+
+// CSS styling
+// 1. Inline CSS
+// 2. External CSS
+// 3. CSS Module
+// 4. CSS in JS: external library => styled-component
+
+const styles = {
+  width: '400px',
+  height: '300px',
+  border: '1px solid red'
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // 1. Inline CSS
+    // <div style={styles}>
+    //   <h1 style={{ color: 'green', fontSize: '100px' }}>Inline CSS</h1>
+    // </div>
+
+    // 2. External CSS
+    // <div className="w-200 h-100 border-green-2 border-red-4">External CSS</div>
+
+    // 3. CSS Module filename: [name].module.css
+    // <div className={classes.test}></div>
+    <div
+      className={`${classes['w-100']} ${classes['h-100']} ${classes['border-yellow-4']}`}
+    ></div>
   );
 }
 
