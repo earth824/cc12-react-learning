29308c91e2a810884681c8fa397eb7a0635bda88
pisutthee <pisut.flyinggiraffe@gmail.com>
2022-09-16 04:39:53 +0700

    completed lesson

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+// localStorage.setItem()
+// localStorage.getItem()
+// localStorage.removeItem()
+// localStorage.clear()
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLogged, setIsLogged] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      setIsLogged(true);
+    }
+  }, []);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post('http://localhost:8007/auth/login', {
+        username,
+        password
+      });
+      console.log(res.data);
+      setIsLogged(true);
+      localStorage.setItem('token', res.data.token);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleClickShow = async () => {
+    try {
+      const res = await axios.get('http://localhost:8007/todos', {
+        headers: {
+          Authorization: 'Bearer ' + localStorage.getItem('token')
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!isLogged ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="username"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+          <input
+            placeholder="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+          <button>Login</button>
+        </form>
+      ) : (
+        <>
+          <h1>Welcome back {username}</h1>
+          <button onClick={handleClickShow}>Show Todo</button>
+        </>
+      )}
     </div>
   );
 }
